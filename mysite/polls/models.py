1187946a095aa87a0d836e4f2856733d28ba1d45
ksg9482 <ksg930523@gmail.com>
2022-06-09 16:45:34 +0900

    tutorial/part5 추가

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -4,24 +4,24 @@ from django.db import models
 from django.utils import timezone
 from django.contrib import admin
 
+#Question 모델
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
 
-    # def was_published_recently(self):
-    #     return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
     @admin.display(
         boolean=True,
         ordering='pub_date',
         description='Published recently?',
     )
-      
+
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
+# Choice 모델
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     # question_id로 외부키가 설정된다.
--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 
 from .models import Question
 
-
+# TestCase의 하위 클래스
 class QuestionModelTests(TestCase):
 
     def test_was_published_recently_with_future_question(self):
@@ -26,11 +26,13 @@ class QuestionModelTests(TestCase):
         recent_question = Question(pub_date=time)
 
         self.assertIs(recent_question.was_published_recently(), True)
+# 이 테스트로 인해 recent_question은 생성부터 23시간59분까지만 '최근'에 생성된 것임을 보장받게 되었다
 
-
+# 이하는 question을 생성하는 함수와 테스트를 생성하는 클래스이다.
 def create_question(question_text, days):
     time = timezone.now() + datetime.timedelta(days=days)
     return Question.objects.create(question_text=question_text, pub_date=time)
+    # 테스트 과정중 question을 생성하는 부분에서 반복 사용된다.
 
 class QuestionIndexViewTests(TestCase):
     def test_no_questions(self):
@@ -38,6 +40,7 @@ class QuestionIndexViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No polls are available.")
         self.assertQuerysetEqual(response.context['latest_question_list'], [])
+        # 연결, 사용가능한 투표가 없음, question리스트가 비어 있음을 확인한다.
 
     def test_past_question(self):
         question = create_question(question_text="Past question.", days=-30)
@@ -46,12 +49,15 @@ class QuestionIndexViewTests(TestCase):
             response.context['latest_question_list'],
             [question],
         )
+        # question을 생성하고 그 question이 리스트에 나타나는지 확인한다.
 
     def test_future_question(self):
         create_question(question_text="Future question.", days=30)
         response = self.client.get(reverse('polls:index'))
         self.assertContains(response, "No polls are available.")
         self.assertQuerysetEqual(response.context['latest_question_list'], [])
+        # 미래의 pub_date로 질문을 생성한다. pub_date가 now보다 크면 filter로 걸러지기 때문에 인덱스에 질문이 없어야 한다.
+        # 데이터베이스는 각 테스트 메서드마다 초기화 되므로 인덱스에 질문이 없어야 한다.
 
     def test_future_question_and_past_question(self):
         question = create_question(question_text="Past question.", days=-30)
@@ -76,10 +82,14 @@ class QuestionDetailViewTests(TestCase):
         future_question = create_question(question_text='Future question.', days=5)
         url = reverse('polls:detail', args=(future_question.id,))
         response = self.client.get(url)
+
         self.assertEqual(response.status_code, 404)
+        # pub_date가 미래인 question에 접근할 경우 404를 리턴한다
 
     def test_past_question(self):
         past_question = create_question(question_text='Past Question.', days=-5)
         url = reverse('polls:detail', args=(past_question.id,))
         response = self.client.get(url)
+
         self.assertContains(response, past_question.question_text)
+        # pub_date가 과거인 question에 접근할 경우 해당 페이지를 리턴한다
